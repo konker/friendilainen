b61f834a10315344e5cf61918a7d4ade1892f3d6
konker.morningwoodsvr1 <konker@gmail.com>
2012-02-16 15:16:16 +0000

    Small change to access_token_helper.sh to parameterize URI

--- access_token_helper.sh
+++ access_token_helper.sh
@@ -2,10 +2,11 @@
 
 APP_ID=`cat app_id.txt`
 APP_SECRET=`cat app_secret.txt`
+URI=http://a-z.fi/
 
-echo "https://www.facebook.com/dialog/oauth?client_id=$APP_ID&redirect_uri=http://a-z.fi/&scope=offline_access"
+echo "https://www.facebook.com/dialog/oauth?client_id=$APP_ID&redirect_uri=$URI&scope=offline_access"
 echo "Enter the code:"
 read CODE
 
-echo "https://graph.facebook.com/oauth/access_token?client_id=$APP_ID&redirect_uri=http://a-z.fi/&client_secret=$APP_SECRET&code=$CODE"
+echo "https://graph.facebook.com/oauth/access_token?client_id=$APP_ID&redirect_uri=$URI&client_secret=$APP_SECRET&code=$CODE"
 
